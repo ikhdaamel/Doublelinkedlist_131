218b1f12f2e2c3f0e10a73817b6266aa7ae889bb
ikhdaamel <ikhda.new45@gmail.com>
2023-05-25 20:04:17 +0700

    implementasi doublelinkedlist ke revtraverse

--- Doublelinkedlist_131/Doublelinkedlist_131.cpp
+++ Doublelinkedlist_131/Doublelinkedlist_131.cpp
@@ -117,3 +117,21 @@ void DoubleLinkedlist::traverse() {
 		}
 	}
 }
+
+void DoubleLinkedlist::revtraverse() {
+	if (listEmpty()) {
+		cout << "\nlist is empty" << endl;
+	}
+
+	else {
+		cout << "\nRecords in descending order of roll number are : " << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+	}
+}
