21e725c67cfae7970569cdd915936f1eba38b2c6
ikhdaamel <ikhda.new45@gmail.com>
2023-05-25 20:19:40 +0700

    implementasi double linked list ke search data

--- Doublelinkedlist_131/Doublelinkedlist_131.cpp
+++ Doublelinkedlist_131/Doublelinkedlist_131.cpp
@@ -149,3 +149,21 @@ void DoubleLinkedlist::hapus(){
 	else
 		cout << "Record with roll number" << rollNo << "deleted" << endl;
 }
+
+void DoubleLinkedlist::searchData() {
+	if (listEmpty() == true) {
+		cout << "\nList is empty" << endl;
+	}
+	Node* prev, * curr;
+	prev = curr = NULL;
+	cout << "\nEnter the roll number of the student whose record you want to search : ";
+	int num;
+	cin >> num;
+	if (DoubleLinkedlist::search(num, &prev, &curr) == false)
+		cout << "\nRecord not found" << endl;
+	else {
+		cout << "\nRecord found" << endl;
+		cout << "\nRoll number :" << curr->noMhs << endl;
+		cout << "\nName : " << curr->name << endl;
+	}
+}
